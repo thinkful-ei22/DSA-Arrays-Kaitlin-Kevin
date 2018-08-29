5f7f8000865c42b5bc5f2af68fa0dadc7a9b0891
Kaitlin Bunn <kabunn1991@gmail.com>
2018-08-29 11:36:13 -0400

    added array drills file

---
+++ array-drills.js
@@ -0,0 +1,15 @@
+const Memory = require('./memory');
+
+function main() {
+  Array.SIZE_RATIO = 3;
+
+  // create an instance of the array class
+  let arr = new Array();
+
+  // add an item to the array
+  arr.push(3);
+
+  console.log(arr);
+}
+
+main();
