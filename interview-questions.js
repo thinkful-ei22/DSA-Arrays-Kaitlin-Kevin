3bb6837f167e0d13f167310fed6c74b77413aa12
Kaitlin Bunn <kabunn1991@gmail.com>
2018-08-29 17:14:14 -0400

    remove characters & products complete

--- interview-questions.js
+++ interview-questions.js
@@ -59,7 +59,8 @@ function sumOfNumbers (arr) {
   let largeSum = 0;
   // loop through and add to sum
   for (let i = 0; i < arr.length; i++) {
-    tempSum = tempSum += arr[i];
+    tempSum += arr[i];
+    // check if tempSum bigger than the largeSum
     if (tempSum > largeSum) {
       largeSum = tempSum;
     }
@@ -71,9 +72,86 @@ sumOfNumbers([4,6,-3,5,-2,1]);
 
 // 4. Merge Arrays
 
+function mergeArrays (arr1, arr2) {
+  let newArray = [];
+  let arr1Index = 0;
+  let arr2Index = 0;
+
+  let newArrayIndex = 0;
+  while (newArrayIndex < (arr1.length + arr2.length) ) {
+
+    let value1 = arr1[arr1Index];
+    let value2 = arr2[arr2Index];
+
+    if (value1 > value2) {
+      newArray[newArrayIndex] = value2;
+      arr2Index++;
+    }
+    else {
+      newArray[newArrayIndex] = value1;
+      arr1Index++;
+    }
+    newArrayIndex++;
+  }
+  console.log(newArray);
+}
+
+mergeArrays([1, 3, 6, 8, 11],[2, 3, 5, 8, 9, 10]);
+
 // 5. Remove Characters
 
+function removeCharas (str, rmv) {
+  let newString = '';
+  // first loop through the string
+  for (let i = 0; i < str.length; i++) {
+  // for each letter loop through the remove and test each letter against and remove it if it's the same  
+  let removeChar = false;
+    for (let j = 0; j < rmv.length; j++) {
+      if (str[i] === rmv[j]) {
+        removeChar = true;
+      } 
+    }
+    if (!removeChar) {
+      newString += (str[i]);
+    }
+  } 
+  console.log(newString);
+}
+
+removeCharas('Battle of the Vowels: Hawaii vs. Grozny', 'aeiou');
+
 // 6. Products
+
+function products (arr) {
+  let productArray = [];
+  // loop through array of numbers
+  for (let i = 0; i < arr.length; i++) {
+    let newArray = [];
+    let removeNum = false;
+    // check each number for the same value
+    for (let j = 0; j < arr.length; j++) {
+      // mark as true for same value
+      if (arr[j] === arr[i]) {
+        removeNum = true;
+      }
+      // 
+      if (!removeNum) {
+        newArray.push(arr[j]);
+      }
+      removeNum = false;
+    }
+    console.log(newArray);
+    let numSum = 1;
+    for (let y = 0; y < newArray.length; y++) {
+      numSum *= newArray[y];
+    }
+    productArray.push(numSum);
+    newArray = [];
+  }
+  console.log(productArray);
+}
+
+products([1, 3, 9, 4]);
 
 // 7. 2D array
 
