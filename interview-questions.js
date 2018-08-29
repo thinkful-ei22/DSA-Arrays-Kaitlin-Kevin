c165100b50ff96d98809c411fd99b2411b14fa5e
Kaitlin Bunn <kabunn1991@gmail.com>
2018-08-29 15:15:16 -0400

    filter array complete

--- interview-questions.js
+++ interview-questions.js
@@ -1,10 +1,12 @@
+// push(), pop(), shift(), unshift(), Math.
+
 // 1. URLify a string
 
 function urlify (str) {
   // loop over it once like an array, forEach does not work
   // target all whitespace in the string
   // change all whitespace to %20
-  
+
   let answer = '';
   for (i = 0; i < str.length; i++) {
     if (str[i] === ' ') {
@@ -31,6 +33,23 @@ function urlify (str) {
 urlify('hello world');
 
 // 2. Filtering an array
+
+function filterArray(arr) {
+  let newArray = [];
+  // loop through array
+  for (let i = 0; i < arr.length; i++) {
+    // compare #'s to 5
+    // remove if 5
+    if (arr[i] >= 5) {
+      newArray.push(arr[i]);
+    } else {
+      // return if not
+    }
+  }
+  console.log(newArray);
+}
+
+filterArray([1,2,3,4,5,6,7,8]);
 
 // 3. Max sum in the array
 
